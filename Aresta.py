e70b165959bcc7ec3b4cccfc96a6bce8c76f6966
Kauan Portela <kauanphbbb@gmail.com>
2021-07-13 14:07:38 -0300

    Iniciando grafo

---
+++ Aresta.py
@@ -0,0 +1,4 @@
+class Aresta:
+    def __init__(self, origin, destination):
+        self.origin = origin
+        self.destination = destination
---
+++ Grafo.py
@@ -0,0 +1,4 @@
+class Fork:
+    def __init__(self):
+        self.verticeList = []
+        self.arestaList = []
